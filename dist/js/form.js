a1277020244b6622291bea7c983b8d96fff67e63
Angel Barrera <angelbarrerasanchez@gmail.com>
2018-04-08 12:44:28 +0200

    adds site

---
+++ dist/js/form.js
@@ -0,0 +1,31 @@
+$('#register').on('click', function (event) {
+    // cancels the form submission
+    event.preventDefault();
+    submitForm();
+});
+
+function submitForm() {
+    // Initiate Variables With Form Content
+    var repository = $("#repository").val();
+    var email = $("#email").val();
+
+    $.ajax({
+        url: "https://api.dev.mygit.website/github/register",
+        type: "POST",
+        data: JSON.stringify({
+            'repository': repository,
+            'email': email
+        }),
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            $("#messages").text('Check your email. You might have a surprise.');
+            $("#response").modal();
+        },
+        error: function (jqxhr) {
+            $("#messages").text(jqxhr.responseJSON.error);
+            $("#response").modal();
+        }
+    })
+
+}
